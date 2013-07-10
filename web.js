dfbba384a764b9360a96b46542670a363cc1df0e
Speed112 <im.speed112@yahoo.com>
2013-07-10 19:24:58 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
 var express = require('express');
 
+var message = fs.readFile('/bitstarter/index.html', function (err, data) {
+if (err) throw err;
+console.log(data);
+});
+
+var buf = new Buffer(256)
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString('utf8', 0, message));
 });
 
 var port = process.env.PORT || 5000;
