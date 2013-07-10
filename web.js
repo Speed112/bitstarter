dfa03e4efd9bec2c75702a7a4b1cde8b4a24f0d8
Speed112 <im.speed112@yahoo.com>
2013-07-10 19:35:43 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,16 +1,12 @@
 var express = require('express');
 
-var message = fs.readFile('/bitstarter/index.html', function (err, data) {
-if (err) throw err;
-console.log(data);
-});
-
-var buf = new Buffer(256)
-
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString('utf8', 0, message));
+
+var fs = require('fs');
+var buf = new Buffer (256);
+  response.send(buf.toString('utc 8', fs.readFileSync("index.html")));
 });
 
 var port = process.env.PORT || 5000;
