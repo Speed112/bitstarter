6f7c3eb18f964e00fb80b40b0336f4d806e5f357
Speed112 <im.speed112@yahoo.com>
2013-07-10 19:41:46 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -6,7 +6,8 @@ app.get('/', function(request, response) {
 
 var fs = require('fs');
 var buf = new Buffer (256);
-  response.send(buf.toString('utc 8', fs.readFileSync("index.html")));
+
+  response.send(buf.toString('utf8', fs.readFileSync("index.html")));
 });
 
 var port = process.env.PORT || 5000;
